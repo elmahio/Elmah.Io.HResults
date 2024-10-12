f097386f9b5002c3513123231c63b2d8c3249207
Magnus Lindhe <magnus@lindhe.org>
2024-10-12 15:56:38 +0200

    Removed unused using directives

--- src/Elmah.Io.HResults/Facilities/FacilityWindowsUpdateResolver.cs
+++ src/Elmah.Io.HResults/Facilities/FacilityWindowsUpdateResolver.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using static System.Net.WebRequestMethods;
-
-namespace Elmah.Io.HResults.Facilities
+﻿namespace Elmah.Io.HResults.Facilities
 {
     internal class FacilityWindowsUpdateResolver : FacilityResolverBase
     {
